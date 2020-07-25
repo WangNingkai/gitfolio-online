5b65116f9980bf0816fce0746a434e7e609b15c9
WangNingkai <1655586865@qq.com>
2020-07-25 20:54:00 +0800

    add cache

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const fetchInfo = require('../src/fetch')
 const renderInfo = require('../src/render')
+const { clampValue, CONSTANTS } = require('../src/utils')
 module.exports = async (req, res) => {
-  const { username, theme } = req.query
+  const { username, theme, cache_seconds } = req.query
   if (typeof theme === 'undefined') {
     themeType = 'dark'
   } else {
@@ -13,6 +14,12 @@ module.exports = async (req, res) => {
   } catch (err) {
     return res.send(err.message)
   }
+  const cacheSeconds = clampValue(
+    parseInt(cache_seconds || CONSTANTS.THIRTY_MINUTES, 10),
+    CONSTANTS.THIRTY_MINUTES,
+    CONSTANTS.ONE_DAY,
+  )
+  res.setHeader('Cache-Control', `public, max-age=${cacheSeconds},s-maxage=86400,stale-while-revalidate`)
   renderInfo(info, { theme: themeType }).then((value) => {
     res.send(value)
   })
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,10 @@ function kFormatter(num) {
   return Math.abs(num) > 999 ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k' : Math.sign(num) * Math.abs(num)
 }
 
+function clampValue(number, min, max) {
+  return Math.max(min, Math.min(number, max))
+}
+
 function request(data, headers) {
   return axios({
     url: 'https://api.github.com/graphql',
@@ -32,6 +36,7 @@ const CONSTANTS = {
 module.exports = {
   kFormatter,
   request,
+  clampValue,
   logger,
   CONSTANTS,
 }
